9acee01f4e2d331efc4eb17c90f5dc9d5296f070
souza10-max <florisgracsouza@gmail.com>
2021-07-20 14:42:47 -0300

    ~/desenvolvedor commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+ 
+ function clicou(){
+     document.getElementById("programador").innerHTML = "<b>obri</b>";
+     //console.log(document.getElementBuId("desenvolvedor"));
+     //alert("desenvolvedor");
+ }
+ 
+ function redirecionar(){
+     window.open("");
+     //window.location.href = "";
+
+ }
+
+ function trocar(elemento){
+    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mause aqui";
+     //alert("trocar text");
+ }
+
+ function voltar(elemento){
+     //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+     elemento.innerHTML = "Passe o mause aqui";
+
+ }  
+ /*
+ function soma(n1,n2){ 
+     return n1 + n2;
+ }*/
+
+ /*
+ var validar;
+ function validaIdade(idade){
+     validar;
+     if (idade >= 18){
+         validar = true
+
+     }else{
+         validar = false
+
+     }
+     return validar;
+ }
+
+ var idade = prompt("idade");
+ validaIdade(idade)
+ console.log(validar);
+*/
+//variavesi  (  var   )
+ /* var nome = "Florisvaldo Graciliano";
+var n1 = 50;
+var n2 = 10;
+var frase = "noite";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toLowerCase());
+*/
+
+
+
